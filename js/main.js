fd131f4d0f41077787b0e5a23a0dd888a28d08d5
celiarodriguezmo <celiro02@gmail.com>
2022-03-22 11:31:21 +0100

    adakiten 3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ jsNewForm.classList.add('js-new-form');
 jsNewForm.classList.remove('collapsed');
 
 
+
+const input_search_desc = document.querySelector('.js_in_search_desc'); 
+
+input_search_desc.value = 'juguetón';
+
+const descrSearchText = input_search_desc.value;
+
+
+
+
 const jsList = document.querySelector('.list');
 jsList.classList.add('js-list');
 
@@ -15,26 +25,29 @@ cardTitle = cardTitle.toUpperCase();
 const cardRace = 'British Shorthair';
 let cardDescription = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
+if( cardDescription.includes(descrSearchText)) {
+  jsList.innerHTML += `<li class="card">
+  <article>
+    <img
+      class="card_img"
+      src="${url}"
+      alt="${alt}"
+    />
+    <h3 class="card_title">${cardTitle}</h3>
+    <h4 class="card_race">${cardRace}</h4>
+    <p class="card_description">${cardDescription}</p>
+  </article>
+  </li>`
+}
+  
 
-jsList.innerHTML += `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="${url}"
-    alt="${alt}"
-  />
-  <h3 class="card_title">${cardTitle}</h3>
-  <h4 class="card_race">${cardRace}</h4>
-  <p class="card_description">${cardDescription}</p>
-</article>
-</li>`
 
 url = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
 cardTitle = 'fiona';
 cardTitle = cardTitle.toUpperCase();
 cardDescription = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
-
+if( cardDescription.includes(descrSearchText)) {
 jsList.innerHTML += `<li class="card">
 <img
   class="card_img"
@@ -45,6 +58,7 @@ jsList.innerHTML += `<li class="card">
 <h4 class="card_race">${cardRace}</h4>
 <p class="card_description">${cardDescription}</p>
 </li>`;
+}
 
 url = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
 cardTitle = 'cielo';
@@ -52,6 +66,7 @@ cardTitle = cardTitle.toUpperCase();
 cardDescription = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
 
+if( cardDescription.includes(descrSearchText)) {
 jsList.innerHTML += `<li class="card">
 <img
   class="card_img"
@@ -62,5 +77,12 @@ jsList.innerHTML += `<li class="card">
 <h4 class="card_race">${cardRace}</h4>
 <p class="card_description">${cardDescription}</p>
 </li>`;
-
+}
 const nameCat = cardTitle.toUpperCase();
+
+jsNewForm.classList.add('collapsed');
+
+const plus = document.querySelector(".menu-nav");
+
+plus.classList.add("plusHidden");
+
